3aba5c5871a7fca0fa0fa6802bead4ac728b57b5
danisaza1 <dani_isaza@htomail.com>
2025-08-19 12:44:31 +0200

    add pag surf

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,15 @@
-import Image from "next/image";
+import Head from "next/head";
 
 export default function Home() {
   return (
-<>
-<head>
-  <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-<meta name="apple-mobile-web-app-capable" content="yes" />
-<meta name="apple-mobile-web-app-status-bar-style" content="default" />
-
-</head>
-<p>Hola</p>
-</>
+    <>
+      <Head>
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      </Head>
+      
+      <p>Hola</p>
+    </>
   );
 }
