977f78510f5b706ec757b127eaf6982b0776cd14
rerefenec <renaud.delorme@free.fr>
2025-08-19 14:09:54 +0200

    autentification frontend

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import Login from "../components/login2";
+
+export default function Home()  {
+return (<>
+ 
+  <div className="bg-blue-200">
+  <p>nouvelle tentative</p>
+  <Login/>
+  </div>
+</>)
+;}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,16 @@
-import Image from "next/image";
+import Head from "next/head";
+import Login from "./components/login2";
 
-export default function Home() {
-  return (
-<>
-<head>
-  <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-<meta name="apple-mobile-web-app-capable" content="yes" />
-<meta name="apple-mobile-web-app-status-bar-style" content="default" />
-
-</head>
-<p>Hola</p>
-</>
-  );
-}
+export default function Home()  {
+return (<>
+  <Head>
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+  </Head>
+  <div className="bg-blue-100">
+  <p>nouvelle tentative</p>
+  <Login/>
+  </div>
+</>)
+;}
