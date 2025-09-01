8e0c9353eab3f899442b4610b4d375fd983eb275
rerefenec <renaud.delorme@free.fr>
2025-09-01 09:50:53 +0200

    création du dossier utils en front et premier commit back

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,59 @@
 const express = require("express");
-
 const app = express();
 
-const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// import express from "express";
+// import pool from "./src/db.js";
+
+const port = 3002;
 
 app.get("/", (req, res) => {
   res.send("Bonjour, monde !");
 });
 
+app.get("/user", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM user");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Erreur serveur");
+  }
+});
+
+// Exemple : ajouter un utilisateur
+app.post("/user", async (req, res) => {
+  const { name, email } = req.body;
+  try {
+    const result = await pool.query(
+      "INSERT INTO users (firstname, lastname, location, username,email, password) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+      [firstname, lastname, location, username, email, password]
+    );
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Erreur serveur");
+  }
+});
+
+app.get("/dog", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM dog");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Erreur serveur");
+  }
+});
+
 app.listen(port, () => {
   console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);
 });
+
+
+
+
+
+
