1cacb8cb22787656f6d53c77b373d35bd6490093
danisaza1 <dani_isaza@htomail.com>
2025-10-08 15:39:04 +0200

    mofidie cors to connect back to front

--- backend/api/app.js
+++ backend/api/app.js
@@ -5,25 +5,33 @@ import getRoutes from "../routes/get.js";
 import authRoutes from "../routes/auth.js";
 import geocodeRouter from "../routes/geocode.js";
 import favoritesRoutes from "../routes/favorites.js";
-// REMOVIDO: import { updateProfile } from "./controllers/authController.js"; 
+// REMOVIDO: import { updateProfile } from "./controllers/authController.js";
 
 const port = process.env.PORT || 3002;
 const app = express();
-const allowedOrigins = ["http://localhost:3000", "http://localhost:3002", "http://localhost:3003", "http://surf-eight-puce.vercel.app"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:3002",
+  "http://localhost:3003",
+  "http://surf-eight-puce.vercel.app",
+  "https://surf-4cpv.vercel.app",
+];
 
 const corsOptions = {
   origin: function (origin, callback) {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true)
+      callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'))
+      callback(new Error("Not allowed by CORS"));
     }
-  }
-}
+  },
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
+  credentials: true, // Allow cookies and authentication headers
+};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors(corsOptions)); 
+app.use(cors(corsOptions));
 
 // Routes
 app.use("/", getRoutes);
@@ -33,10 +41,9 @@ app.use("/", authRoutes);
 // Las rutas de geocodificación, perfil (GET/PUT /profile) y favoritos (GET/POST/DELETE /favorites)
 // están ahora bajo el prefijo '/api'.
 app.use("/api", geocodeRouter);
-app.use("/api", favoritesRoutes)
-// REMOVIDO: app.put("/profile", updateProfile); 
-
+app.use("/api", favoritesRoutes);
+// REMOVIDO: app.put("/profile", updateProfile);
 
 app.listen(port, () => {
-  console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);
+  console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);
 });
